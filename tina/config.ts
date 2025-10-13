afb31aba31b8c61aa096b2410c20b4a97d26be9a
Henrik Schönfelder <mail@henrikschoenfelder.de>
2025-10-13 14:26:16 +0200

    update

--- tina/config.ts
+++ tina/config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       {
         name: "post",
         label: "Posts",
-        path: "content/posts",
+        path: "content/german/blog",
         fields: [
           {
             type: "string",
